54ed9d1bc09e775096962520470b0dc6aa94ce03
tangzero <t4ngz3r0@gmail.com>
2009-03-03 18:03:39 -0300

    fixes on spec tests

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,7 +1,6 @@
-=begin
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe RyBOSS::Request do
+describe RGSearch::Request do
 
 	setup do		
 		@connection = mock("connection")
@@ -11,22 +10,22 @@ describe RyBOSS::Request do
 	context 'without proxy' do
 		
 		setup do
-			RyBOSS.proxy_host = nil
-			RyBOSS.proxy_port = nil
+			RGSearch.proxy_host = nil
+			RGSearch.proxy_port = nil
 		end
 
 		it 'should make requests'	do
-			RyBOSS::Request.get('http://www.google.com.br').should_not be_nil
+			RGSearch::Request.get('http://www.google.com.br').should_not be_nil
 		end
 		
 		it 'should make requests with parameters' do
-			RyBOSS::Request.get('http://www.google.com.br/search', :q => 'java').should_not be_nil
+			RGSearch::Request.get('http://www.google.com.br/search', :q => 'java').should_not be_nil
 		end
 
 		it 'should has an error when trying to access nonexistent urls' do
 			@connection.should_receive(:start).with(any_args()).and_raise(@exception)
 			Net::HTTP.should_receive(:Proxy).with(any_args()).and_return(@connection)
-			lambda { RyBOSS::Request.get('http://foo.bar.com') }.should raise_error
+			lambda { RGSearch::Request.get('http://foo.bar.com') }.should raise_error
 		end
 
 	end
@@ -34,28 +33,27 @@ describe RyBOSS::Request do
 	context 'with proxy' do
 
 		setup do
-			RyBOSS.proxy_host = 'proxy.foo.intranet'
-			RyBOSS.proxy_port = '1234'
+			RGSearch.proxy_host = 'proxy.foo.intranet'
+			RGSearch.proxy_port = '1234'
 			
 			# comment this line when test real proxys
-			RyBOSS.should_receive(:proxy_host).and_return(nil)
+			RGSearch.should_receive(:proxy_host).and_return(nil)
 		end
 
 		it 'should make requests'	do
-			RyBOSS::Request.get('http://www.google.com.br').should_not be_nil
+			RGSearch::Request.get('http://www.google.com.br').should_not be_nil
 		end
 		
 		it 'should make requests with parameters' do
-			RyBOSS::Request.get('http://www.google.com.br/search', :q => 'java').should_not be_nil
+			RGSearch::Request.get('http://www.google.com.br/search', :q => 'java').should_not be_nil
 		end
 		
 		it 'should has an error when trying to access nonexistent urls' do
 			@connection.should_receive(:start).with(any_args()).and_raise(@exception)
 			Net::HTTP.should_receive(:Proxy).with(any_args()).once.and_return(@connection)
-			lambda { RyBOSS::Request.get('http://foo.bar.com') }.should raise_error
+			lambda { RGSearch::Request.get('http://foo.bar.com') }.should raise_error
 		end
 
 	end
 
 end
-=end
